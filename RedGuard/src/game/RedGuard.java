4081ecb7f572283a6aaa244bcf68a38e9b9154ae
thephonatic22 <davidmallette22@gmail.com>
2017-05-17 09:20:38 -0400

    Updated Comment

--- RedGuard/src/game/RedGuard.java
+++ RedGuard/src/game/RedGuard.java
@@ -1,10 +1,13 @@
 package game;
 
+/* David Mallette, Michael Gorley
+ * Grade 12 Computer Science CPT
+ */
+
 public class RedGuard {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
